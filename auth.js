a67aafb2901e111bb4e56fdbfa2d055dcf8a921d
Ken Lee <66231048+swekenlee12@users.noreply.github.com>
2021-05-27 12:56:21 -0600

    add login snippets

---
+++ auth.js
@@ -0,0 +1,21 @@
+//login user
+const loginForm = document.querySelector('');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+    loginForm.querySelector('.error').innerHTML = '';
+  }).catch(err => {
+    loginForm.querySelector('.error').innerHTML = err.message;
+  });
+
+});
